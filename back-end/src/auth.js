4b87825f7b15c0b944a102ea10243d2f966bdc66
Kelvin Musselli <kelvin.serva@afya.com.br>
2020-09-29 22:35:37 -0300

    First commit

---
+++ back-end/src/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+const authConfig = {
+  secret: "c1892b69f41ccbc88ebf15a9e05d7cc0",
+  expiresIn: "7d",
+};
+
+export default async (req, res, next) => {
+  const authHeaders = req.headers.authorization;
+
+  if (!authHeaders) {
+    return res.status(401).json({ error: 'Token não enviado!' });
+  }
+
+  const [, token] = authHeaders.split(' ');
+
+  try {
+    const decodificarsenha = await promisify(jwt.verify)(
+      token,
+      authConfig.secret
+    );
+
+    req.userId = decodificarsenha.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalido!' });
+  }
+};
